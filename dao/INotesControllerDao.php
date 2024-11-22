702131edf99ac8892553084e286c9fd6b1b9766f
GabrielFf2 <149058439+GabrielFf2@users.noreply.github.com>
2024-11-22 16:06:50 +0100

    interfaceDao

---
+++ dao/INotesControllerDao.php
@@ -0,0 +1,12 @@
+<?php
+
+interface INotesControllerDao
+{
+    public function create();
+    public function destroy();
+    public function edit();
+    public function index();
+    public function show();
+    public function store();
+    public function update();
+}
